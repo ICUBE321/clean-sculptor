40c4cd2bbc829a4b19ad382d3beef585c4b1c811
Izien <izieniremiren@gmail.com>
2024-07-27 10:50:13 -0400

    KAN-2 implemented homepage design. Currently using hardcoded itams to make styling. No functionality yet in the searchbar

--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -3,7 +3,7 @@ import SearchFood from "../components/SearchFood";
 
 const SearchPage = () => {
   return (
-    <div className="f">
+    <div className="max-w-full mx-auto flex justify-center items-center">
       <SearchFood />
     </div>
   );
