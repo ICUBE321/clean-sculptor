f23fa0d9f4e5690032b89f8c0ba2de27a22d85b0
Lois Aleghe <loisaleghe@gmail.com>
2024-10-22 00:13:17 -0400

    skipped list earlier

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -73,7 +73,7 @@ const Navbar = () => {
                     ? "text-darkblue hover:text-darkblue"
                     : "text-lightblue md:hover:text-darkblue md:hover:bg-transparent hover:text-white hover:bg-transparent";
                   style +=
-                    " block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0";
+                    " block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0";
                   return style;
                 }}
               >
