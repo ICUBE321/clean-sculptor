4f28ee548d02a16eb24a6b91dbb63bf0e8b1cb92
Izien <izieniremiren@gmail.com>
2024-11-18 20:45:21 -0500

    KAN-6  wip updated server

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ const port = 3000;
 
 // cors middleware
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", process.env.ALLOW_CLIENT);
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
