f2c79dbe97f562161a8d05dda4043900a0063f1f
Izien <izieniremiren@gmail.com>
2024-11-18 20:51:47 -0500

    KAN-6 WIP

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 
 const user = require("./routes/user");
 const food = require("./routes/food");
-const port = 3000;
+const port = process.env.PORT;
 
 // cors middleware
 app.use((req, res, next) => {
